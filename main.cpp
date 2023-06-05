8cdf98de9937abb48d38126f7858a7cba0421c83
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2023-06-05 13:35:18 +0900

    初期設定

--- main.cpp
+++ main.cpp
@@ -1,1 +1,40 @@
 ﻿#include "DxLib.h"
+
+#define _SCREEN_HEIGHT_ 720
+#define _SCREEN_WIDHT_	1280
+#define _SCREEN_COLOR_BIT_16_ 16
+#define _SCREEN_COLOR_BIT_32_ 32
+
+
+int WINAPI WinMain(_In_ HINSTANCE ih, _In_opt_ HINSTANCE ioh, _In_ LPSTR il, _In_ int ii)
+{
+    // ウィンドウモードで起動
+    if (ChangeWindowMode(TRUE) != DX_CHANGESCREEN_OK)
+    {
+        return -1;
+    }
+
+    // 画面サイズを変更
+    if (SetGraphMode(_SCREEN_WIDHT_, _SCREEN_HEIGHT_, _SCREEN_COLOR_BIT_32_) != DX_CHANGESCREEN_OK)
+    {
+        return -1;
+    }
+
+    // DXライブラリの初期化
+    if (DxLib_Init() == -1)
+    {
+        return -1;
+    }
+
+    while (ProcessMessage() != -1)
+    {
+        ClearDrawScreen();
+
+        ScreenFlip();
+    }
+
+    // DXライブラリの終了処理
+    DxLib_End();
+
+    return 0;
+}
