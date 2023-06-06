834b15142a044c5496944e4224bfe994a4e49d5c
Kanato Shimabukuro <kanato.shimabukuro@gmail.com>
2023-06-06 12:43:34 +0900

    Update

--- SourceFiles/TitleScene.cpp
+++ SourceFiles/TitleScene.cpp
@@ -10,25 +10,32 @@ Title::Title() {
     state = 0;
     ctrl_state = 0;
 
-    img_title = 0;
+    // 画像読み込み
+    if ((img_title = LoadGraph("Resources/Images/title.png")) == -1) {};
 
+    // フォント読み込み
     font[0][0] = CreateFontToHandle(NULL, 128, -1, DX_FONTTYPE_NORMAL);
     font[0][1] = CreateFontToHandle(NULL, 64, -1, DX_FONTTYPE_NORMAL);
     font[0][2] = CreateFontToHandle(NULL, 32, -1, DX_FONTTYPE_NORMAL);
     font[0][3] = CreateFontToHandle(NULL, 16, -1, DX_FONTTYPE_NORMAL);
-
     LPCSTR font_path = "Resources/Fonts/syokakiutage.ttf"; // 読み込むフォントファイルのパス
     if (AddFontResourceEx(font_path, FR_PRIVATE, NULL) < 0) {
         // フォント読込エラー処理
         MessageBox(NULL, "フォント読込失敗", "", MB_OK);
     };
-
     font[1][0] = CreateFontToHandle("しょかきうたげ（無料版）", 128, 9, DX_FONTTYPE_EDGE);
     font[1][1] = CreateFontToHandle("しょかきうたげ（無料版）", 64, 9, DX_FONTTYPE_EDGE);
     font[1][2] = CreateFontToHandle("しょかきうたげ（無料版）", 32, 9, DX_FONTTYPE_EDGE);
     font[1][3] = CreateFontToHandle("しょかきうたげ（無料版）", 16, 9, DX_FONTTYPE_EDGE);
 
-    if ((img_title = LoadGraph("Resources/Images/title.png")) == -1) {};
+    // サウンド読み込み
+    bgm = LoadSoundMem("Resources/Sounds/bgm_title.wav");
+    se_cursor = LoadSoundMem("Resources/Sounds/se_cursor.wav");
+    se_select = LoadSoundMem("Resources/Sounds/se_select.wav");
+
+    ChangeVolumeSoundMem(255, bgm);
+    ChangeVolumeSoundMem(130, se_cursor);
+    ChangeVolumeSoundMem(150, se_select);
 };
 
 Title::~Title() {
@@ -52,7 +59,7 @@ AbstractScene* Title::Update() {
     };
     if ((InputControl::TipLeftLStick(STICKL_Y) > 100 && ctrl_state == 0) || (CheckHitKey(KEY_INPUT_UP) && ctrl_state == 0)) {
         // カーソル上
-        //PlaySoundMem(Sound::GetSounds(SND_SE_CURSOR), DX_PLAYTYPE_BACK, TRUE);
+        PlaySoundMem(se_cursor, DX_PLAYTYPE_BACK, TRUE);
         if (state <= 0) {
             state = 3;
         }
@@ -63,7 +70,7 @@ AbstractScene* Title::Update() {
     }
     else if ((InputControl::TipLeftLStick(STICKL_Y) < -32000 && ctrl_state == 0) || (CheckHitKey(KEY_INPUT_DOWN) && ctrl_state == 0)) {
         // カーソル下
-        //PlaySoundMem(Sound::GetSounds(SND_SE_CURSOR), DX_PLAYTYPE_BACK, TRUE);
+        PlaySoundMem(se_cursor, DX_PLAYTYPE_BACK, TRUE);
         if (state >= 3) {
             state = 0;
         }
@@ -74,7 +81,7 @@ AbstractScene* Title::Update() {
     };
 
     if (InputControl::PressBotton(XINPUT_BUTTON_B) || CheckHitKey(KEY_INPUT_SPACE)) {
-        //if (CheckSoundMem(Sound::GetSounds(SND_SE_SELECT)) == 0) PlaySoundMem(Sound::GetSounds(SND_SE_SELECT), DX_PLAYTYPE_BACK, TRUE);
+        if (CheckSoundMem(se_select) == 0) PlaySoundMem(se_select, DX_PLAYTYPE_BACK, TRUE);
         if (state == 0) {
             // スタート選択
             return new GameMain();
@@ -127,5 +134,10 @@ void Title::Draw() const {
     }
     else if (state == 3) {
         DrawStringToHandle(570, 500, "→", 0x000000, font[1][2], 0xffffff);
+    };
+
+    // BGM 再生
+    if (CheckSoundMem(bgm) == 0) {
+        PlaySoundMem(bgm, DX_PLAYTYPE_LOOP, TRUE);
     };
 };
