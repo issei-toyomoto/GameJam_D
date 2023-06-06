676e4502125c1a241230bdc51f7c3acc281c5692
arikawaaaaaaaaa <taisei-arikawa-k21h@stu.kbc.ac.jp>
2023-06-06 17:34:09 +0900

    プレイヤー・鎌を振る

--- SourceFiles/player.h
+++ SourceFiles/player.h
@@ -5,6 +5,13 @@ private:
     float X, Y;   //À•W
     float SpdX, SpdY;   //‘¬“x
     float MSpdX, MSpdY;   //Å‚‘¬“x
+    float Angle = 0;          //ˆÚ“®•ûŒü(-180`180)
+    int Way = 0;               //UŒ‚•ûŒü(1`8)
+
+    int Attack = 0;     //UŒ‚ŠÇ—
+    int Combo = 0;      //UŒ‚‰ñ”
+
+    int SickleIng;  //Š™‚Ì‰æ‘œ
 
 public:
     Player();
