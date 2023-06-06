56be2191cc77b1484818da6d4fa67e8883c76a79
arikawaaaaaaaaa <taisei-arikawa-k21h@stu.kbc.ac.jp>
2023-06-06 09:30:30 +0900

    common

--- SourceFiles/GameMainScene.cpp
+++ SourceFiles/GameMainScene.cpp
@@ -1,3 +1,4 @@
+#include "common.h"
 #include "main.h"
 #include "Result.h"
 #include "PadInput.h"
@@ -23,6 +24,18 @@ void GameMain::Draw() const { // やることは描画のみ、絶対に値の�
     SetFontSize(16);
     DrawGraph(0, 0, BackImg, true);
     DrawBox(0, 0, 1280, 100, GetColor(0, 0, 0), TRUE);
+
+    for (int i = 100; i < 720; i+=BLOCK_SIZE)
+    {
+        DrawLine(0, i, 1280, i, 0xffffff);
+        if ((i - 100) / BLOCK_SIZE % 5 == 0)DrawLine(0, i, 1280, i, 0xff0000);
+    }
+    for (int i = 0; i < 1280; i += BLOCK_SIZE)
+    {
+        DrawLine(i, 100, i, 720, 0xffffff);
+        if (i / BLOCK_SIZE % 5 == 0)DrawLine(i, 100, i, 720, 0xff0000);
+    }
+
     DrawFormatString(20, 120, 0xffffff, "Game Main");
     DrawFormatString(1130, 10, 0xffffff, "SCORE : %d", score);
 };
