54732b699b13a620a5d5dde5042d41fa770adc65
katoushiki <106945582+katoushiki@users.noreply.github.com>
2023-06-07 09:47:42 +0900

    ランキング画面制作途中

---
+++ ranking.cpp
@@ -0,0 +1,18 @@
+﻿#include "ranking.h"
+#include"DxLib.h"
+ranking::ranking()
+{
+   /* ranking[i];*/
+    i = 0;
+    RANKING_MAX;
+}
+void ranking::Update()
+{
+
+}
+void ranking::Draw()const
+{
+    for (int i = 0; i < RANKING_MAX; i++) {
+        DrawFormatString(120, 100 + (i * 30), GetColor(255, 255, 255), "%d 位　 %.1f 秒", i + 1, Ranking[i]);
+    }
+}
