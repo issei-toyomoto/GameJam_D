373035b8aa16fc1b05c4d47a930d9f18f542ea87
Kanato Shimabukuro <120351493+KanatoShimabukuro@users.noreply.github.com>
2023-06-08 09:17:59 +0900

    Update

--- SourceFiles/DrawRanking.cpp
+++ SourceFiles/DrawRanking.cpp
@@ -1,22 +1,23 @@
 ﻿
 #include"main.h"
-DrawRanking::DrawRanking()
-{
-           /* ranking[i];*/
-            /*i = 0;*/
-            Ranking::ReadRanking();
-}
-DrawRanking::~DrawRanking()
-{
+DrawRanking::DrawRanking() {
 
-}
-AbstractScene* DrawRanking::Update()
-{
+    Ranking::ReadRanking();
+
+};
+
+DrawRanking::~DrawRanking() {
+
+};
+
+AbstractScene* DrawRanking::Update() {
     return this;
-}
-void DrawRanking::Draw()const
-{
+};
+
+void DrawRanking::Draw()const {
+
     for (int i = 0; i < RANK_MAX; i++) {
-        DrawFormatString(120, 100 + (i * 30), GetColor(255, 255, 255), "%d 位　 %s　%7d", Ranking::GetData(i).no, Rank,Ranking::GetData(i).name,Ranking::GetData(i).score);
-    }
-}
+        DrawFormatString(120, 100 + (i * 30), GetColor(255, 255, 255), "%d 位　 %s　%7d", Ranking::GetData(i).no, Ranking::GetData(i).name, Ranking::GetData(i).score);
+    };
+
+};
--- SourceFiles/DrawRanking.h
+++ SourceFiles/DrawRanking.h
@@ -1,17 +1,13 @@
 #pragma once
-#include "AbstractScene.h"
-#include"Ranking.h"
-class DrawRanking: public AbstractScene {
-private:
-    int i;
-    int  RANKING_MAX = 10;
-    int Rank[10];
 
-    Ranking ranking;
+#include "AbstractScene.h"
+
+class DrawRanking : public AbstractScene {
 
 public:
     DrawRanking();
     ~DrawRanking();
+
     // •`‰æˆÈŠO‚ÌXV‚ğÀ‘•
     virtual AbstractScene* Update() override;
 
